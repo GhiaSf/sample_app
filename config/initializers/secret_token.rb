b78bf9123acd7abd21334fbf4445e2c2ff4425a5
GhiaSf <petesmail79@gmail.com>
2014-02-01 18:33:58 -0800

    Add precompiled assets for Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '94e8539fd7835c532e30c5300fee72a3a8a8204b81f0c27291c25515eb613ebd3d972480a53fae975b4f9388ccdd82f235b8b3a7e79bbd0b6e21ab5f8b9fdbbc'
